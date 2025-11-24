e8244cf675a38ec6b1a68c5934df6f38952976f1
r3hmantom <r3hmantom@gmail.com>
2025-11-24 05:13:22 +0500

    add: test route

--- apps/api/src/routes/index.js
+++ apps/api/src/routes/index.js
@@ -1,21 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.use('/', (req,res,next)=>{
-    console.log(req)
-    next();
-})
+router.use("/", (req, res, next) => {
+  console.log(req);
+  next();
+});
 
-const memoryRoutes = require('./memories');
-const plannerRoutes = require('./planner');
-const authRoutes = require('./auth');
-const dashboardRoutes = require('./dashboard');
-const userRoutes = require('./user');
+const memoryRoutes = require("./memories");
+const plannerRoutes = require("./planner");
+const authRoutes = require("./auth");
+const dashboardRoutes = require("./dashboard");
+const userRoutes = require("./user");
 
-router.use('/auth', authRoutes);
-router.use('/memories', memoryRoutes);
-router.use('/planner', plannerRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/user', userRoutes);
+router.use("/auth", authRoutes);
+router.use("/memories", memoryRoutes);
+router.use("/planner", plannerRoutes);
+router.use("/dashboard", dashboardRoutes);
+router.use("/user", userRoutes);
+app.get("/health", (req, res) => res.json({ status: "ok" }));
 
 module.exports = router;
